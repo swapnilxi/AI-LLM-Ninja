609537631f383e953890bb0aa5ca03f7e9b53877
Swapnil Gupta <swapnilgupta.xi@gmail.com>
2025-09-10 23:35:54 +0530

    circle-on -thumb

--- Vision-RAG/visionrag-backend/GestureDetection/HandRecognition.py
+++ Vision-RAG/visionrag-backend/GestureDetection/HandRecognition.py
@@ -18,7 +18,26 @@ while True:
     #multiple hands
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id , lm in enumerate(handLms.landmark):
+                #print(id,lm)
+                h,w,c= img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
+                #comment id for all
+                if id == 4:
+                    cv2.circle(img, (cx,cy), 15, (255,0,255), cv2.FILLED) 
             mpDraw.draw_landmarks(img, handLms,mp.hands.HAND_CONNECTIONS)
+            
+
+#hand_frame_rate
+    ctime = time.time()
+    fps = 1/(ctime - ptime)
+    ptime = ctime
+    
+   # cv2.putText(img, text, position, fontFace, fontScale, color[, thickness) 
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    
+    
             
     
     cv2.imshow("Image", img)
