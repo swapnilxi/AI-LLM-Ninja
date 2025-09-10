31f95f7b2f6e62989ec8e0ba5ea897847d64c215
Swapnil Gupta <swapnilgupta.xi@gmail.com>
2025-09-10 23:04:12 +0530

    hand-detection-mediapipe

---
+++ Vision-RAG/visionrag-backend/GestureDetection/HandRecognition.py
@@ -0,0 +1,25 @@
+import cv2
+import mediapipe as mp
+import numpy as np 
+import time 
+
+cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw= mp.solutions.drawing_utils  
+
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    #print(results.multi_hand_landmarks)
+    
+    #multiple hands
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms,mp.hands.HAND_CONNECTIONS)
+            
+    
+    cv2.imshow("Image", img)
+    cv2.waitKey(1) 
